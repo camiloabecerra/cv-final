e6db2e0cbc722889e6688ff7331fa91af37e4a90
Seyoung <seyoung_jang@brown.edu>
2024-12-15 15:02:37 -0500

    testing

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ import os
 from ultralytics import YOLO
 from hyperparameters import EPOCHS, IMGSZ
 
-data_path = "data/data.yaml"
+data_path = "datasets/data/data.yaml"
 
 model = YOLO("yolo11n.pt")
 results = model.train(data=data_path, epochs=EPOCHS, imgsz=IMGSZ)
--- testing.py
+++ testing.py
@@ -7,7 +7,7 @@ results = model(TEST_IMG_PATH)
 
 # Plot predictions
 fig, ax = plt.subplots(figsize=(12, 8))
-# ax.imshow(cv2.cvtColor(results[0].plot(), cv2.COLOR_BGR2RGB))
+ax.imshow(cv2.cvtColor(results[0].plot(), cv2.COLOR_BGR2RGB))
 ax.axis("off")
 
 plt.show()
