7db83936b650ada3c5f761871f420cc572afb87e
camiloabecerra <camilo_becerra@brown.edu>
2024-12-16 19:46:08 -0500

    improvements to k means

--- annotations.py
+++ annotations.py
@@ -3,26 +3,34 @@ import cv2
 from team_assignments import Detector
 
 model = YOLO("finetuned.pt")
-path = "videos/video2.mov"
+path = "videos/video3.mov"
 capture = cv2.VideoCapture(path)
 
 width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = int(capture.get(cv2.CAP_PROP_FPS))
 
-output = cv2.VideoWriter("annotated_videos/video2_annotated.mp4", cv2.VideoWriter_fourcc(*'mp4v'), fps, (width,height))
+output = cv2.VideoWriter("annotated_videos/video3_annotated.mp4", cv2.VideoWriter_fourcc(*'mp4v'), fps, (width,height))
 ball_pos = []
+teams = []
 
 while capture.isOpened():
     ret, frame = capture.read()
     if not ret:
         break
 
-    classifier = Detector(frame, model)
+    if teams == []:
+        classifier = Detector(frame, model)
+    else:
+        classifier = Detector(frame, model, teams)
+
     ball_pos += classifier.ball
     classifier.annotate_img()
 
-    output.write(classifier.img)
+    if teams == []:
+        teams = classifier.teams
+
+    output.write(classifier.img[:,:,0])
 
 capture.release()
 output.release()
--- team_assignments.py
+++ team_assignments.py
@@ -5,29 +5,27 @@ import numpy as np
 import math
 
 class Detector:
-    def __init__(self, img, model, team_centroids=None):
+    def __init__(self, img, model, team_centroids=[]):
         self.img = img
-        self.detection = model.predict(self.img, conf=0.1)[0]
         self.teams = team_centroids
+        self.detection = model.predict(self.img, conf=0.1)[0]
+        self.players = list(filter(lambda elt: self.detection.names[int(elt[5])] == "player", self.detection.boxes.data))
+        self.ball = list(filter(lambda elt: self.detection.names[int(elt[5])] == "ball", self.detection.boxes.data))
 
-        names = self.detection.names
-        self.players = list(filter(lambda elt: names[int(elt[5])] == "player", self.detection.boxes.data))
-        self.ball = list(filter(lambda elt: names[int(elt[5])] == "ball", self.detection.boxes.data))
-
-    def classify(self):
-        return self.detection
+        if len(self.players) >= 2:
+            self.team_players = self.assign_teams()
+        else:
+            self.team_players = {}
 
     def assign_teams(self):
-        if not self.teams:
+        avg_colors = list(map(lambda p: np.mean(self.img[int(p[1]):int(p[3]),int(p[0]):int(p[2])], axis=(0,1,2)), self.players))
 
-
-        avg_colors = []
-        for player in self.players:
-            val = np.mean(self.img[int(player[1]):int(player[3]),int(player[0]):int(player[2])])
-            avg_colors.append(val)
-
-        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 15, 1.0)
-        _,labels,_ = cv2.kmeans(np.float32(avg_colors), 2, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 50, 1.0)
+        if len(self.teams) == 0:
+            _,labels,self.teams = cv2.kmeans(np.float32(avg_colors), 2, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+            self.teams = [self.teams[0][0],self.teams[1][0]]
+        else:
+            labels = list(map(lambda x: 0 if math.dist([self.teams[0]],[x]) <= math.dist([self.teams[1]],[x]) else 1, avg_colors))
 
         team0 = []
         team1 = []
@@ -49,10 +47,9 @@ class Detector:
         axes_len = (int(math.dist([int(detection[0])],[int(detection[2])])), 30)
 
         color = (0,0,255)
-        if len(self.players) >= 2:
-            if detection in self.assign_teams()[0]:
-                color = (255,0,0)
-            elif detection in self.assign_teams()[1]:
-                color = (0,255,0)
+        if detection in self.team_players[0]:
+            color = (255,0,0)
+        elif detection in self.team_players[1]:
+            color = (0,255,0)
 
-        self.img = cv2.ellipse(self.img, center_coords, axes_len, 0, 45, 255, color, 4)
+        self.img = cv2.ellipse(self.img, center_coords, axes_len, 0, 35, 235, color, 4)
